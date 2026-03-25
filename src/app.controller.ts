7a45719bc8d9a45a8ff1b984c7a667a87f3046b1
event <event@z2t11c3.42bangkok.com>
2026-03-25 11:17:03 +0700

    00

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {AppModule} from './app.module'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      success: true,
+      data: {
+        service: "purchase-api",
+        version: "1.0.0",
+      },
+    };
   }
 }
